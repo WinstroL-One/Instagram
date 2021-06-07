36f6c44ede8a72d1a7acf54254557599abbcfb17
GODO <d.iocancsgo@gmail.com>
2021-06-07 10:51:01 +0200

    Test

---
+++ index.php
@@ -0,0 +1,197 @@
+<?php
+/*
+Mencuri adalah termasuk perbuatan yang keji, karena ia mengambil sesuatu milik orang lain 
+yang bukan menjadi hak dirinya dan ia telah mendzaliminya
+
+الكبائر هي ما رتب عليه عقوبة خاصة بمعنى أنها ليست مقتصرة على مجرد النهي أو التحريم، بل لا بد من عقوبة خاصة مثل أن يقال من فعل هذا فليس بمؤمن، أو فليس منا، أو ما أشبه ذلك، هذه هي الكبائر، والصغائر هي المحرمات التي ليس عليها عقوبة
+“Dosa besar adalah yang Allah ancam dengan suatu hukuman khusus. Maksudnya perbuatan tersebut tidak sekedar dilarang atau diharamkan, 
+namun diancam dengan suatu hukuman khusus. Semisal disebutkan dalam dalil ‘barangsiapa yang melakukan ini maka ia bukan mukmin’, 
+atau ‘bukan bagian dari kami’, atau semisal dengan itu. Ini adalah dosa besar. Dan dosa kecil adalah dosa yang tidak diancam 
+dengan suatu hukuman khusus” (Fatawa Nurun ‘alad Darbi libni Al-‘Utsaimin, 2/24, Asy-Syamilah).
+
+وَالسَّارِقُ وَالسَّارِقَةُ فَاقْطَعُوا أَيْدِيَهُمَا جَزَاءً بِمَا كَسَبَا نَكَالًا مِنَ اللَّهِ وَاللَّهُ عَزِيزٌ حَكِيمٌ
+“Laki-laki yang mencuri dan perempuan yang mencuri, potonglah tangan keduanya (sebagai) pembalasan 
+bagi apa yang mereka kerjakan dan sebagai siksaan dari Allah. Dan Allah Maha Perkasa lagi Maha Bijaksana” (QS. Al Maidah: 38).
+
+Mencuri Adalah Dosa Besar
+
+Allah Ta’ala berfirman:
+
+وَالسَّارِقُ وَالسَّارِقَةُ فَاقْطَعُوا أَيْدِيَهُمَا جَزَاءً بِمَا كَسَبَا نَكَالًا مِنَ اللَّهِ وَاللَّهُ عَزِيزٌ حَكِيمٌ
+
+“Laki-laki yang mencuri dan perempuan yang mencuri, potonglah tangan keduanya (sebagai) pembalasan bagi apa yang mereka kerjakan dan sebagai siksaan dari Allah. Dan Allah Maha Perkasa lagi Maha Bijaksana” (QS. Al Maidah: 38).
+
+Dalam ayat ini, Allah Ta’ala menetapkan hukuman hadd bagi pencuri adalah dipotong tangannya. Ini menunjukkan bahwa mencuri adalah dosa besar. Syaikh Muhammad bin Shalih Al-‘Utsaimin mengatakan:
+
+الكبائر هي ما رتب عليه عقوبة خاصة بمعنى أنها ليست مقتصرة على مجرد النهي أو التحريم، بل لا بد من عقوبة خاصة مثل أن يقال من فعل هذا فليس بمؤمن، أو فليس منا، أو ما أشبه ذلك، هذه هي الكبائر، والصغائر هي المحرمات التي ليس عليها عقوبة
+
+“Dosa besar adalah yang Allah ancam dengan suatu hukuman khusus. Maksudnya perbuatan tersebut tidak sekedar dilarang atau diharamkan, namun diancam dengan suatu hukuman khusus. Semisal disebutkan dalam dalil ‘barangsiapa yang melakukan ini maka ia bukan mukmin’, atau ‘bukan bagian dari kami’, atau semisal dengan itu. Ini adalah dosa besar. Dan dosa kecil adalah dosa yang tidak diancam dengan suatu hukuman khusus” (Fatawa Nurun ‘alad Darbi libni Al-‘Utsaimin, 2/24, Asy-Syamilah).
+
+Ibnu Shalah rahimahullah mengatakan:
+
+لَهَا أَمَارَات مِنْهَا إِيجَاب الْحَدّ , وَمِنْهَا الْإِيعَاد عَلَيْهَا بِالْعَذَابِ بِالنَّارِ وَنَحْوهَا فِي الْكِتَاب أَوْ السُّنَّة , وَمِنْهَا وَصْف صَاحِبهَا بِالْفِسْقِ , وَمِنْهَا اللَّعْن
+
+“Dosa besar ada beberapa indikasinya, diantaranya diwajibkan hukuman hadd kepadanya, juga diancam dengan azab neraka atau semisalnya, di dalam Al-Qur’an dan As-Sunnah. Demikian juga, pelakunya disifati dengan kefasikan dan laknat ” (Tafsir Ibnu Katsir, 2/285).
+
+Pencuri Mendapat Laknat
+
+Pencuri juga dilaknat oleh Allah Ta’ala. Rasulullah Shallallahu’alaihi Wasallam bersabda:
+
+لعن الله السارق يسرق البيضة فتقطع يده ويسرق الحبل فتقطع يده
+
+“Allah melaknat pencuri yang mencuri sebutir telur, lalu di lain waktu ia dipotong tangannya karena mencuri tali.” (HR. Bukhari no. 6285).
+
+Syaikh Muhammad bin Shalih Al-‘Utsaimin menjelaskan:
+
+أن يراد بذلك أن هذا السارق قد يسرق البيضة فتهون السرقة في نفسه، ثم يسرق ما يبلغ النصاب فيقطع
+
+“Maksud hadits ini adalah seorang yang mencuri telur lalu dia menganggap remeh perbuatan tersebut sehingga kemudian ia mencuri barang yang melewati nishab hadd pencurian, sehingga ia dipotong tangannya” (Syarhul Mumthi‘, 14/336-337).
+
+Mencuri Adalah Kezaliman
+
+Dan secara umum mencuri termasuk perbuatan mengambil harta orang lain dengan cara batil. Padahal harta seorang Muslim itu haram. Rasulullah Shallallahu’alaihi Wasallam bersabda:
+
+فَإِنَّ اللَّهَ حَرَّمَ عَلَيْكُمْ دِمَاءَكُمْ وَأَمْوَالَكُمْ وَأَعْرَاضَكُمْ كَحُرْمَةِ يَوْمِكُمْ هَذَا ، فِي شَهْرِكُمْ هَذَا، فِي بَلَدِكُمْ هَذَا
+
+“Sesungguhnya Allah telah mengharamkan atas sesama kalian darah kalian (untuk ditumpakan) dan harta kalian (untuk dirampais) dan kehormatan (untuk dirusak). Sebagaimana haramnya hari ini, haramnya bulan ini dan haramnya negeri ini” (HR. Bukhari no. 1742).
+
+Dan mencuri juga termasuk perbuatan zalim. Padahal Allah Ta’ala berfirman:
+
+أَلاَ لَعْنَةُ اللّهِ عَلَى الظَّالِمِينَ
+
+“Ingatlah, laknat Allah (ditimpakan) atas orang-orang yang zalim” (QS. Hud: 18).
+
+وَكَذَلِكَ أَخْذُ رَبِّكَ إِذَا أَخَذَ الْقُرَى وَهِيَ ظَالِمَةٌ إِنَّ أَخْذَهُ أَلِيمٌ شَدِيدٌ
+
+“Dan begitulah azab Tuhanmu, apabila Dia mengazab penduduk negeri-negeri yang berbuat zalim. Sesungguhnya azab-Nya itu adalah sangat pedih lagi keras.” (QS. Hud: 102).
+
+إِنَّهُ لاَ يُفْلِحُ الظَّالِمُونَ
+
+“Sesungguhnya orang-orang yang zalim itu tidak mendapat keberuntungan” (QS. Al An’am: 21).
+
+Nabi Shallallahu’alaihi Wasallam bersabda:
+
+قال الله تبارك وتعالى: يا عبادي، إني حرمت الظلم على نفسي، وجعلته بينكم محرمًا؛ فلا تظالموا
+
+“Allah Tabaaraka wa ta’ala berfirman: ‘Wahai hambaku, sesungguhnya aku haramkan kezaliman atas Diriku, dan aku haramkan juga kezaliman bagi kalian, maka janganlah saling berbuat zalim‘.” (HR. Muslim no. 2577).
+
+Hukuman Hadd Bagi Pencuri
+
+Berdasarkan surat Al Maidah ayat 38 di atas, hukuman hadd bagi pencuri dalam Islam adalah di potong tangannya. Juga berdasarkan hadits dari ‘Aisyah radhiallahu’anha, beliau berkata:
+
+أنَّ قريشًا أهمَّهم شأنُ المرأةِ المخزوميَّةِ التي سرقت في عهدِ النبيِّ صلَّى اللهُ عليه وسلَّمَ . في غزوةِ الفتحِ . فقالوا : من يُكلِّمُ فيها رسولَ اللهِ صلَّى اللهُ عليه وسلَّمَ ؟ فقالوا : ومن يجترئُ عليه إلا أسامةُ بنُ زيدٍ ، حِبُّ رسولِ اللهِ صلَّى اللهُ عليه وسلَّمَ ؟ فأتى بها رسولَ اللهِ صلَّى اللهُ عليه وسلَّمَ . فكلَّمه فيها أسامةُ بنُ زيدٍ . فتلوَّنَ وجهُ رسولِ اللهِ صلَّى اللهُ عليه وسلَّمَ . فقال ( أتشفعُ في حدٍّ من حدودِ اللهِ ؟ ) فقال له أسامةُ : استغفِرْ لي . يا رسولَ اللهِ ! فلما كان العشيُّ قام رسولُ اللهِ صلَّى اللهُ عليه وسلَّمَ فاختطب . فأثنى على اللهِ بما هو أهلُه . ثم قال ( أما بعد . فإنما أهلك الذين مَن قبلكم ، أنهم كانوا إذا سرق فيهم الشريفُ ، تركوه . وإذا سرق فيهم الضعيفُ ، أقاموا عليه الحدَّ . وإني ، والذي نفسي بيدِه ! لو أنَّ فاطمةَ بنتَ محمدٍ سرقت لقطعتُ يدَها ) ثم أمر بتلك المرأةِ التي سرقتْ فقُطعَتْ يدُها . …قالت عائشةُ : فحسنُتْ توبتُها بعد . وتزوَّجتْ . وكانت تأتيني بعد ذلك فأرفعُ حاجتَها إلى رسولِ اللهِ صلَّى اللهُ عليه وسلَّمَ
+
+“Bahwa orang-orang Quraisy pernah digemparkan oleh kasus seorang wanita dari Bani Mahzum yang mencuri di masa Rasulullah Shallallahu’alaihi Wasallam tepatnya ketika masa perang Al Fath. Lalu mereka berkata: “Siapa yang bisa berbicara dengan Rasulullah Shallallahu’alaihi Wasallam? Siapa yang lebih berani selain Usamah bin Zaid, orang yang dicintai Rasulullah Shallallahu’alaihi Wasallam?”. Maka Usamah bin Zaid pun menyampaikan kasus tersebut kepada Rasulullah Shallallahu’alaihi Wasallam, hingga berubahlah warna wajah Rasulullah. Lalu beliau bersabda: “Apakah kamu hendak memberi syafa’ah (pertolongan) terhadap seseorang dari hukum Allah?”. Usamah berkata: “Mohonkan aku ampunan wahai Rasulullah”. Kemudian sore harinya Rasulullah Shallallahu’alaihi Wasallam berdiri seraya berkhutbah. Beliau memuji Allah dengan pujian yang layak bagi-Nya, kemudian bersabda: “Amma ba’du. Sesungguhnya sebab hancurnya umat sebelum kalian adalah bahwa mereka itu jika ada pencuri dari kalangan orang terhormat, mereka biarkan. Dan jika ada pencuri dari kalangan orang lemah, mereka tegakkan hukum pidana. Adapun aku, demi Dzat yang jiwaku berada di tangan-Nya, jika Fatimah bintu Muhammad mencuri maka akan aku potong tangannya”. Lalu Rasulullah memerintahkan wanita yang mencuri tersebut untuk dipotong tangannya. Aisyah berkata:”Setelah itu wanita tersebut benar-benar bertaubat, lalu menikah. Dan ia pernah datang kepadaku setelah peristiwa tadi, lalu aku sampaikan hajatnya kepada Rasulullah Shallallahu’alaihi Wasallam.” (HR. Al Bukhari 3475, 4304, 6788, Muslim 1688, dan ini adalah lafadz Muslim).
+
+Namun tidak dikenai hukuman potongan tangan jika:
+
+Barang yang dicuri nilainya kecil. Rasulullah Shallallahu’alaihi Wasallam bersabda:
+لاَ تُقْطَعُ يَدُ السَّارِقِ إِلاَّ فِي رُبْعِ دِيْنَارٍ فَصَاعِدًا
+“Pencuri tidak dipotong tangannya kecuali barang yang dicuri senilai seperempat dinar atau lebih.” (Muttafaqun ‘alahi).
+Yang ini disebut juga sebagai nisab pencurian.
+Barang yang dicuri bukan sesuatu yang disimpan dalam tempat penyimpanan. Rasulullah Shallallahu’alaihi Wasallam bersabda:
+لا تقطع اليد في تمر معلق
+“Tidak dipotong tangan pencuri bila mencuri kurma yang tergantung.” (HR. Ibnu Hazm dalam Al Muhalla 11/323, dihasankan Al Albani dalam Shahih Al Jaami’ 7398)
+Syaikh As Sa’di menjelaskan:
+
+ومن سرق ربع دينار من الذهب، أو ما يساويه من المال من حرزه : قطعت يده اليمنى من مفصل الكف، وحسمت فإن عاد قطعت رجله اليسرى من مفصل الكعب وحسمت فإن عاد حبس
+
+“Orang yang mencuri 4 dinar emas (atau lebih) atau yang senilai dengan itu, dari tempat penyimpanannya, maka ia dipotong tangannya yang kanan mulai dari pergelangan tangan. Kemudian dihentikan pendarahannya. Jika ia mengulang lagi, maka dipotong kakinya yang kiri dari mata kakinya. Kemudian dihentikan pendarahannya. Jika mengulang lagi, maka dipenjara.” (Minhajus Salikin, 231-232).
+
+Adapun jika mencurinya tidak sampai nisab pencurian, sehingga ia tidak dipotong tangan, maka hukumannya adalah ta’zir. Ta’zir adalah hukuman yang ditentukan oleh ijtihad hakim, bisa jadi berupa penjara, hukuman cambuk, hukuman kerja sosial atau lainnya. Syaikh As Sa’di menjelaskan:
+
+التعزير واجب في كل معصية لا حد فيه و لا كفارة
+
+“Ta’zir hukumnya wajib bagi semua maksiat yang tidak ada hadd-nya dan tidak ada kafarahnya” (Minhajus Salikin, 231).
+
+Harta Hasil Mencuri Tidak Halal
+
+Rasulullah Shallallahu’alaihi Wasallam bersabda:
+
+كل لحم نبت من سحت فالنار أولى به
+
+“Setiap daging yang tumbuh dari suhtun, maka api neraka lebih layak baginya” (HR. Ahmad no. 14481, dishahihkan Al Albani dalam Shahih Al Jami no. 4519).
+
+Makna suhtun adalah:
+
+السُّحُتُ: كلُّ حرام قبيح الذِّكر؛ وقيل: هو ما خَبُثَ من المَكاسب وحَرُم فلَزِمَ عنه العارُ
+
+“As suhtu adalah semua yang haram dan buruk untuk disebutkan. Sebagian mengatakan artinya: setiap penghasilan yang buruk dan haram serta layak dicela.” (Lisaanul ‘Arab).
+
+Bertaubat Dari Mencuri, Harus Kembalikan Barang Curiannya
+
+Ibnul Qayyim rahimahullah mengatakan,
+
+مَنْ قَبَضَ مَا لَيْسَ لَهُ قَبْضُهُ شَرْعًا، ثُمَّ أَرَادَ التَّخَلُّصَ مِنْهُ، فَإِنْ كَانَ الْمَقْبُوضُ قَدْ أُخِذَ بِغَيْرِ رِضَى صَاحِبِهِ، وَلَا اسْتَوْفَى عِوَضَهُ رَدَّهُ عَلَيْهِ. فَإِنْ تَعَذَّرَ رَدُّهُ عَلَيْهِ، قَضَى بِهِ دَيْنًا يَعْلَمُهُ عَلَيْهِ، فَإِنْ تَعَذَّرَ ذَلِكَ، رَدَّهُ إِلَى وَرَثَتِهِ، فَإِنْ تَعَذَّرَ ذَلِكَ، تَصَدَّقَ بِهِ عَنْهُ، فَإِنِ اخْتَارَ صَاحِبُ الْحَقِّ ثَوَابَهُ يَوْمَ الْقِيَامَةِ، كَانَ لَهُ. وَإِنْ أَبَى إِلَّا أَنْ يَأْخُذَ مِنْ حَسَنَاتِ الْقَابِضِ، اسْتَوْفَى مِنْهُ نَظِيرَ مَالِهِ، وَكَانَ ثَوَابُ الصَّدَقَةِ لِلْمُتَصَدِّقِ بِهَا
+
+“Orang yang mengambil barang orang lain tanpa dibenarkan oleh syariat, kemudian ia ingin bertaubat, maka jika pemiliknya tidak ridha dan tidak mau menerima ganti rugi, barang tersebut wajib dikembalikan. Jika sudah tidak bisa dikembalikan, maka menjadi beban hutang yang wajib diberitahukan kepada pemiliknya. Jika tidak bisa ditunaikan kepada pemiliknya, maka wajib ditunaikan kepada ahli warisnya. Jika tidak bisa pula, maka disedekahkan atas nama pemiliknya” (Zaadul Ma’ad, 5/690).
+
+Pencuri Akan Diqishash Di Hari Kiamat
+
+Orang yang mencuri harta orang lain, yang ia belum bertaubat serta belum mengembalikan atau mengganti barang curiannya, maka ia akan dituntut oleh orang tersebut di hari kiamat. Dari Abu Hurairah radhiallahu ‘anhu, bahwa Nabi shallallahu ‘alaihi wa sallam pernah bertanya:
+
+أتدرون ما المفلِسُ ؟ قالوا : المفلِسُ فينا من لا درهمَ له ولا متاعَ . فقال : إنَّ المفلسَ من أمَّتي ، يأتي يومَ القيامةِ بصلاةٍ وصيامٍ وزكاةٍ ، ويأتي قد شتم هذا ، وقذف هذا ، وأكل مالَ هذا ، وسفك دمَ هذا ، وضرب هذا . فيُعطَى هذا من حسناتِه وهذا من حسناتِه . فإن فَنِيَتْ حسناتُه ، قبل أن يقضيَ ما عليه ، أخذ من خطاياهم فطُرِحت عليه . ثمَّ طُرِح في النَّارِ
+
+“Tahukah kalian siapa orang yang bangkrut?”. Para shahabat pun menjawab, ”Orang yang bangkrut menurut kami adalah orang yang tidak memiliki uang dirham maupun harta benda”. Nabi bersabda, ”Sesungguhnya orang yang bangkrut di kalangan umatku adalah orang yang datang pada hari kiamat dengan membawa pahala shalat, puasa, dan zakat, tetapi ia juga datang membawa dosa berupa perbuatan mencela, menuduh, memakan harta orang lain, menumpahkan darah, dan memukul orang lain. Kelak kebaikan-kebaikannya akan diberikan kepada orang yang terzalimi. Apabila amalan kebaikannya sudah habis diberikan, sementara belum selesai pembalasan tindak kezalimannya, maka diambillah dosa-dosa orang yang terzalimi itu, lalu diberikan kepadanya. Kemudian dia pun dicampakkan ke dalam neraka.” (HR. Muslim no. 2581).
+
+Nabi shallallahu ‘alaihi wa sallam bersabda:
+
+مَنْ كَانَتْ لَهُ مَظْلَمَةٌ لِأَخِيهِ مِنْ عِرْضِهِ أَوْ شَيْءٍ فَلْيَتَحَلَّلْهُ مِنْهُ الْيَوْمَ قَبْلَ أَنْ لَا يَكُونَ دِينَارٌ وَلَا دِرْهَمٌ إِنْ كَانَ لَهُ عَمَلٌ صَالِحٌ أُخِذَ مِنْهُ بِقَدْرِ مَظْلَمَتِهِ وَإِنْ لَمْ تَكُنْ لَهُ حَسَنَاتٌ أُخِذَ مِنْ سَيِّئَاتِ صَاحِبِهِ فَحُمِلَ عَلَيْهِ
+
+“Siapa yang pernah berbuat aniaya (zhalim) terhadap kehormatan saudaranya atau sesuatu apapun hendaklah dia meminta kehalalannya (maaf) pada hari ini (di dunia) sebelum datang hari yang ketika itu tidak bermanfaat dinar dan dirham. Jika dia tidak lakukan, maka (nanti pada hari kiamat) bila dia memiliki amal shalih akan diambil darinya sebanyak kezholimannya. Apabila dia tidak memiliki kebaikan lagi maka keburukan saudaranya yang dizhaliminya itu akan diambil lalu ditimpakan kepadanya”. (HR. Al-Bukhari no. 2449)
+
+Semoga Allah Ta’ala memberi kita taufik agar kita dijauhkan dari perbuatan mencuri harta orang lain.
+
+*/
+echo "Versi 1.2\nUpdate Date 23/02/2019\n\n";
+echo "[?] Masukan Jumlah: ";
+$loop = trim(fgets(STDIN));
+echo "[?] Masukan nama Gmail : ";
+$email = trim(fgets(STDIN));
+echo "[?] Masukan Password Kamu: ";
+$password = trim(fgets(STDIN));
+echo "\n";
+for ($x = 0; $x < $loop; $x++) {
+    echo "[>] Sedang membuat akun.....";
+    sleep(5);
+    echo "OK\n";
+	echo "[>] Mengisi Nama Random......";
+    sleep(3);
+    echo "OK\n";
+    echo "[>] Mengisi Tanggal Lahir Random......";
+    sleep(3);
+    echo "OK\n";
+    echo "[>] Mengisi Email Sesuai Nama Gmail Kamu diatas......";
+    sleep(3);
+    echo "OK\n";
+    echo "[>] Mengisi Password Kamu Sesuai Password diatas......";
+    sleep(3);
+    echo "OK\n";
+    echo "[>] Berhasil Membuat Gmail $email......";
+    sleep(2);
+    echo "Success\n";
+    echo "[>] Menunggu Prosess Verifikasi Nomor HP......";
+    sleep(20);
+    echo "Suksess\n";
+    echo "[>] Nomor hp +62821887xxx......";
+    sleep(5);
+    echo "Success\n";
+    echo "[>] Mengunggu Pesan Kode Verifikasi......";
+    sleep(10);
+    echo "Success\n";
+    echo "[>] Suksess Kode verifikasi : 956126......";
+    sleep(5);
+    echo "Success\n";
+    echo "[>] Prosess Memasukkan Kode Verifikasi......";
+    sleep(5);
+    echo "Succes\n";
+    echo "[>] Verifikasi Suksess......";
+    sleep(2);
+    echo "Success\n";
+    echo "[>] Menyiapkan Akun Google......";
+    sleep(10);
+    echo "[>] Akun Gmail Telah Berhasil Dibuat......";
+    sleep(4);
+    echo "Success\n\n";
+}
+echo "[=] DONE";
